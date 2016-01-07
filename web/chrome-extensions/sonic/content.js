24ab53640857d816b74b806b88ff6e5fcf4b618f
HaraldNordgren <haraldnordgren@gmail.com>
2016-01-07 08:05:54 +0100

    chrome-extensions: query box on page can be found and filled, but not in newly opened window

--- web/chrome-extensions/sonic/content.js
+++ web/chrome-extensions/sonic/content.js
@@ -1,1 +1,21 @@
-alert(document.getElementsByClassName("question-hyperlink")[0]);
+var raw_title = document.getElementsByClassName("question-hyperlink")[0];
+
+var query_box = document.getElementsByName("q")[0];
+query_box.value = raw_title;
+
+//alert(document.getElementsByName("q"));
+//alert(query_box);
+
+
+var kickass = window.open("https://kickass.unblocked.pe/");
+
+/*
+kickass.onload = function () {
+    document.write("5");
+    //kickass.document.write(kickass.document.getElementById("contentSearch"));
+};
+*/
+
+
+//alert(kickass.document.getElementById("contentSearch"));
+//kickass.document.getElementById("contentSearch")[0].value = raw_title;
