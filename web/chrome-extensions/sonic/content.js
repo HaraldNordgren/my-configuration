60a7c7ac2f3bf3711b57468a7db6646e610fed83
HaraldNordgren <haraldnordgren@gmail.com>
2016-01-07 10:17:08 +0100

    chrome-extensions: jquery works

--- web/chrome-extensions/sonic/content.js
+++ web/chrome-extensions/sonic/content.js
@@ -1,7 +1,9 @@
+//$(document).ready(function () { alert("Hello World"); });
+
 var raw_title = document.getElementsByClassName("question-hyperlink")[0];
 
-var query_box = document.getElementsByName("q")[0];
-query_box.value = raw_title;
+//var query_box = document.getElementsByName("q")[0];
+//query_box.value = raw_title;
 
 //alert(document.getElementsByName("q"));
 //alert(query_box);
@@ -9,13 +11,20 @@ query_box.value = raw_title;
 
 var kickass = window.open("https://kickass.unblocked.pe/");
 
+$(kickass.document).ready( function() {
+    //alert(kickass.innerWidth);
+    //$("#contentSearch")
+    kickass.alert("hej");
+    var s = kickass.document.getElementById("contentSearch");
+    alert(s);
+});
+
 /*
 kickass.onload = function () {
     document.write("5");
     //kickass.document.write(kickass.document.getElementById("contentSearch"));
 };
 */
-
 
 //alert(kickass.document.getElementById("contentSearch"));
 //kickass.document.getElementById("contentSearch")[0].value = raw_title;
