8c635987bc3e990f8998b4e4d7854de31d2dd3fb
HaraldNordgren <haraldnordgren@gmail.com>
2016-02-04 03:05:51 +0100

    updated init-repo-and-checkout-folder script

--- git-scripts/init-repo-and-checkout-folder.sh
+++ git-scripts/init-repo-and-checkout-folder.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
-    echo "usage: repo-url [folder]"
+    echo "usage: $0 <repo-url> <folder>"
     exit 1
 fi
 
 git init
 git remote add -f origin $1
 
-if [ -n "$2" ]
-then
-    git config core.sparseCheckout true
-    echo $2 >> .git/info/sparse-checkout
-fi
+git config core.sparseCheckout true
+echo $2 >> .git/info/sparse-checkout
 
 git pull origin master
