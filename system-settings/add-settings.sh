3c318164001ac00c7e25664556449bbf94a25a5a
HaraldNordgren <haraldnordgren@gmail.com>
2016-06-20 05:34:57 +0200

    Upated UI of add-settings script

--- system-settings/add-settings.sh
+++ system-settings/add-settings.sh
@@ -2,48 +2,68 @@
 
 set -e
 
+sudo true 1> /dev/null
+
 function hardlink {
-    ln $1 $2 -f
-    chown $SUDO_USER:$SUDO_USER $2
+    sudo ln $1 $2 -f
+    sudo chown $USER:$USER $2
     echo "Hardlinked $1"
 }
 
-function append {
-    cat $1 >> $2
-    echo "Appended to $2"
-}
+#function append {
+#    cat $1 >> $2
+#    echo "Appended to $2"
+#}
 
-# Make sure only root can run the script
-if [[ $EUID -ne 0 ]]
-then
-   echo "Must run as root!" 1>&2
-   exit 1
+
+if [[ $(uname -s) == Linux ]]; then
+    cd $(dirname $(readlink -f $0))
 fi
 
-# Go to script directory
-SCRIPT=$(readlink -f $0)
-cd $(dirname $SCRIPT)
-
-BASHRC=$HOME/.bashrc
-BASH_ALIASES=$HOME/.bash_aliases
-PROFILE=$HOME/.profile
-VIMRC=$HOME/.vimrc
-GCONF_BASE=$HOME/.gconf
-GCONF=$GCONF_BASE/apps/gnome-terminal
-
-hardlink settings-data/.bashrc $BASHRC
-hardlink settings-data/.bash_aliases $BASH_ALIASES
-hardlink settings-data/.vimrc $VIMRC
+echo "HARDLINKING CONFIG FILES"
 hardlink settings-data/.zshrc $HOME/.zshrc
-hardlink settings-data/.gitignore $HOME/.gitignore && git config --global core.excludesfile ~/.gitignore
+hardlink settings-data/.vimrc $HOME/.vimrc
+echo
 
-append settings-data/.profile $PROFILE
-chown $SUDO_USER:$SUDO_USER $PROFILE
+echo "SETTING GIT CONFIG"
+hardlink settings-data/.gitignore $HOME/.gitignore
+git config --global core.excludesfile ~/.gitignore
+git config --global user.email "haraldnordgren@gmail.com"
+git config --global user.name "HaraldNordgren"
+echo
 
-mkdir -p $GCONF
-cp -p --parents keybindings/%gconf.xml $GCONF
-chown $SUDO_USER:$SUDO_USER $GCONF_BASE -R
-echo "Added keybindings to $GCONF"; echo
+#hardlink settings-data/.bashrc $HOME/.bashrc
 
-echo "Sucessfully installed config!"
-echo "Log in and out to reload gconf and profile"
+if [[ $(uname -s) == Linux ]] && [[ "$(uname -v)" == *"Ubuntu"* ]]; then
+
+    #hardlink settings-data/.bash_aliases $HOME/.bash_aliases
+
+    #PROFILE=$HOME/.profile
+    #append settings-data/.profile $PROFILE
+    #chown $SUDO_USER:$SUDO_USER $PROFILE
+
+    UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+
+    if [[ $UBUNTU_VERSION == 14.04 ]]; then
+        APT="apt-get"
+
+        echo "ADDING GCONF KEYBINDINGS"
+        GCONF_BASE=$HOME/.gconf
+        GCONF=$GCONF_BASE/apps/gnome-terminal
+        mkdir -p $GCONF
+        cp -p --parents keybindings/%gconf.xml $GCONF
+        #chown $SUDO_USER:$SUDO_USER $GCONF_BASE -R
+        echo "Log in and out to reload Gconf"
+        echo
+
+    elif [[ $UBUNTU_VERSION == 16.04 ]]; then
+        APT="apt"
+    fi
+
+    echo "INSTALLING SOFTWARE PACKAGES"
+    sudo $APT update
+    sudo $APT install -y qbittorrent vim vlc zsh postgresql
+    echo
+fi
+
+echo "SUCCESSFULLY INSTALLED CONFIG"
