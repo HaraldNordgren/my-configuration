df7d11282eba302b0bf3731ef6f8bb1252fa7600
HaraldNordgren <haraldnordgren@gmail.com>
2016-06-25 01:02:30 +0200

    Improved the output of add-settings

--- system-settings/add-settings.sh
+++ system-settings/add-settings.sh
@@ -5,34 +5,43 @@ set -e
 sudo true 1> /dev/null
 
 function hardlink {
-    sudo ln $1 $2 -f
+    if [ ! -f $1 ]; then return; fi
+    sudo ln -f $1 $2
     sudo chown $USER:$USER $2
-    echo "Hardlinked $1"
+    echo "Hardlinked $(basename $1 | sed 's|^\.||') to $2"
 }
 
-#function append {
-#    cat $1 >> $2
-#    echo "Appended to $2"
-#}
+function symlink {
+    if [ ! -d $1 ]; then return; fi
+    dest=$2/$(basename $1)
+    rm -f $dest
+    sudo ln -s $1 $2
+    sudo chown $USER:$USER $2
+    echo "Symlinked $(basename $1) to $dest"
+}
 
 
 if [[ $(uname -s) == Linux ]]; then
     cd $(dirname $(readlink -f $0))
 fi
 
-echo "HARDLINKING CONFIG FILES"
+echo "LINKING CONFIG FILES"
 hardlink settings-data/.zshrc $HOME/.zshrc
 hardlink settings-data/.vimrc $HOME/.vimrc
+#hardlink settings-data/.bashrc $HOME/.bashrc
 echo
 
-echo "SETTING GIT CONFIG"
+echo "SETTING GIT CONFIG LINKING REPOSITORIES"
 hardlink settings-data/.gitignore $HOME/.gitignore
-git config --global core.excludesfile ~/.gitignore
+git config --global --replace-all core.excludesfile ~/.gitignore
+git config --global push.default simple
 git config --global user.email "haraldnordgren@gmail.com"
 git config --global user.name "HaraldNordgren"
+
+symlink $PWD ~
+(cd ../.. && symlink my-resume ~)
 echo
 
-#hardlink settings-data/.bashrc $HOME/.bashrc
 
 if [[ $(uname -s) == Linux ]] && [[ "$(uname -v)" == *"Ubuntu"* ]]; then
 
@@ -45,16 +54,15 @@ if [[ $(uname -s) == Linux ]] && [[ "$(uname -v)" == *"Ubuntu"* ]]; then
     UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
 
     if [[ $UBUNTU_VERSION == 14.04 ]]; then
-        APT="apt-get"
-
         echo "ADDING GCONF KEYBINDINGS"
         GCONF_BASE=$HOME/.gconf
         GCONF=$GCONF_BASE/apps/gnome-terminal
         mkdir -p $GCONF
         cp -p --parents keybindings/%gconf.xml $GCONF
-        #chown $SUDO_USER:$SUDO_USER $GCONF_BASE -R
         echo "Log in and out to reload Gconf"
         echo
+
+        APT="apt-get"
 
     elif [[ $UBUNTU_VERSION == 16.04 ]]; then
         APT="apt"
@@ -62,12 +70,13 @@ if [[ $(uname -s) == Linux ]] && [[ "$(uname -v)" == *"Ubuntu"* ]]; then
 
     echo "INSTALLING SOFTWARE PACKAGES"
     sudo $APT update
-    sudo $APT install -y qbittorrent vim vlc zsh postgresql libgnome-keyring-dev
+    sudo $APT install -y qbittorrent vim vlc zsh postgresql
     echo
 
     echo "SETTING UP GNOME KEYRING FOR GIT"
+    sudo $APT install -y libgnome-keyring-dev
     cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
+    sudo make 1> /dev/null
     git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
     echo
 
